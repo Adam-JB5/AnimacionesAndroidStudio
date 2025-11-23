34ccc9b0a7ab60414c3e97f75fb0bcc34c0c9428
adam.janah <adam.janah@geoaitech.com>
2025-11-23 18:52:02 +0100

    Animacion modificada

--- app/src/main/java/com/example/animaciones/AnimationHelper.kt
+++ app/src/main/java/com/example/animaciones/AnimationHelper.kt
@@ -11,10 +11,14 @@ class AnimationHelper {
         val rotar = ObjectAnimator.ofFloat(vista, "rotation", 0f, 360f)
         val escalarX = ObjectAnimator.ofFloat(vista, "scaleX", 1f, 1.3f, 1f)
         val escalarY = ObjectAnimator.ofFloat(vista, "scaleY", 1f, 1.3f, 1f)
+        val opacidad = ObjectAnimator.ofFloat(vista, "alpha", 1f, 0f, 1f)
+
+        //La animacion no dejara de reproducirse
+        rotar.repeatCount = ObjectAnimator.INFINITE
 
         val animacion = AnimatorSet()
-        animacion.playTogether(moverY, rotar, escalarX, escalarY)
-        animacion.duration = 1500
+        animacion.playTogether(moverY, rotar, escalarX, escalarY, opacidad)
+        animacion.duration = 3000
         animacion.start()
     }
 }
